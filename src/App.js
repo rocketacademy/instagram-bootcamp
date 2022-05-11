a6cfb7fb067eef750cf8dd97b1d2ac3327e44d2b
neokaiyuan <kai@rocketacademy.co>
2022-05-12 01:11:16 +0800

    Posts base ref soln

--- src/App.js
+++ src/App.js
@@ -1,63 +1,109 @@
 import React from "react";
-import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { onChildAdded, push, ref as databaseRef, set } from "firebase/database";
+import {
+  getDownloadURL,
+  ref as storageRef,
+  uploadBytes,
+} from "firebase/storage";
+import Card from "react-bootstrap/Card";
+import { database, storage } from "./firebase";
 import logo from "./logo.png";
 import "./App.css";
 
-// Save the Firebase message folder name as a constant to avoid bugs due to misspelling
-const MESSAGE_FOLDER_NAME = "messages";
+// Save Firebase folder names as constants to avoid bugs due to misspelling
+const IMAGES_FOLDER_NAME = "images";
+const POSTS_FOLDER_NAME = "posts";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
+    // Initialise empty posts array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
-      messages: [],
+      posts: [],
+      fileInputFile: null,
+      fileInputValue: "",
       textInputValue: "",
     };
   }
 
   componentDidMount() {
-    const messagesRef = ref(database, MESSAGE_FOLDER_NAME);
+    const postsRef = databaseRef(database, POSTS_FOLDER_NAME);
     // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
+    onChildAdded(postsRef, (data) => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
       this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
+        // Store post key so we can use it as a key in our list items when rendering posts
+        posts: [...state.posts, { key: data.key, val: data.val() }],
       }));
     });
   }
 
-  handleChange = (event) => {
+  handleFileInputChange = (event) => {
+    this.setState({
+      fileInputFile: event.target.files[0],
+      fileInputValue: event.target.value,
+    });
+  };
+
+  handleTextInputChange = (event) => {
     this.setState({ textInputValue: event.target.value });
   };
 
   // Note use of array fields syntax to avoid having to manually bind this method to the class
   handleSubmit = (event) => {
     event.preventDefault();
-    const postListRef = ref(database, MESSAGE_FOLDER_NAME);
-    const newPostRef = push(postListRef);
-    set(newPostRef, this.state.textInputValue);
-    // Reset input field after submit
-    this.setState({ textInputValue: "" });
+
+    // Store images in an images folder in Firebase Storage
+    const fileRef = storageRef(
+      storage,
+      `${IMAGES_FOLDER_NAME}/${this.state.fileInputFile.name}`
+    );
+
+    // Upload file, save file download URL in database with post text
+    uploadBytes(fileRef, this.state.fileInputFile).then(() => {
+      getDownloadURL(fileRef).then((downloadUrl) => {
+        const postListRef = databaseRef(database, POSTS_FOLDER_NAME);
+        const newPostRef = push(postListRef);
+        set(newPostRef, {
+          imageLink: downloadUrl,
+          text: this.state.textInputValue,
+        });
+        // Reset input field after submit
+        this.setState({
+          fileInputFile: null,
+          fileInputValue: "",
+          textInputValue: "",
+        });
+      });
+    });
   };
 
   render() {
-    // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val}</li>
+    // Convert posts in state to post JSX elements to render
+    let postCards = this.state.posts.map((post) => (
+      <Card key={post.key}>
+        <Card.Img src={post.val.imageLink} className="Card-Img" />
+        <Card.Text>{post.val.text}</Card.Text>
+      </Card>
     ));
+    // Reverse the order of posts such that newest posts are on top
+    postCards.reverse();
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <form onSubmit={this.handleSubmit}>
             <input
+              type="file"
+              value={this.state.fileInputValue}
+              onChange={this.handleFileInputChange}
+            />
+            <br />
+            <input
               type="text"
               value={this.state.textInputValue}
-              onChange={this.handleChange}
+              onChange={this.handleTextInputChange}
             />
             <input
               type="submit"
@@ -66,7 +112,8 @@ class App extends React.Component {
               disabled={!this.state.textInputValue}
             />
           </form>
-          <ol>{messageListItems}</ol>
+          <br />
+          {postCards}
         </header>
       </div>
     );
