d8ade159ae7627b80a4519cc642d23232071bad9
neokaiyuan <kai@rocketacademy.co>
2022-05-14 17:44:51 +0800

    Save Rocket's solution for reference solution usage

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,26 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// TODO: Replace with your app's Firebase project configuration
+// TODO: Replace Rocket's reference solution config with your app's Firebase project configuration
+// const firebaseConfig = {
+//   apiKey: "API_KEY",
+//   authDomain: "PROJECT_ID.firebaseapp.com",
+//   // The value of `databaseURL` depends on the location of the database
+//   databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app",
+//   projectId: "PROJECT_ID",
+//   storageBucket: "PROJECT_ID.appspot.com",
+//   messagingSenderId: "SENDER_ID",
+//   appId: "APP_ID",
+// };
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyA9k-F40SzVpAWWSKpICi2tqbYJrJVqiho",
+  authDomain: "instagram-5e863.firebaseapp.com",
+  databaseURL:
+    "https://instagram-5e863-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "instagram-5e863",
+  storageBucket: "instagram-5e863.appspot.com",
+  messagingSenderId: "1016013432700",
+  appId: "1:1016013432700:web:9ecc9fe184c5786eb0e6f9",
 };
 
 // Initialize Firebase
