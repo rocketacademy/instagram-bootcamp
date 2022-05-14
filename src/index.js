53d7f9391611b5b5047b212c28096ce82e5a46f6
neokaiyuan <kai@rocketacademy.co>
2022-05-14 17:46:15 +0800

    Fix duplicate import of Bootstrap CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
