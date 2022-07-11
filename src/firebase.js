867821fcf622e962d454e120041655921449f16a
adamvader <adamonglt@hotmail.com>
2022-07-11 14:37:50 +0800

    initial setup for rocketgram

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyBjU5Ml0DtWGnzi4e-6wGDDHGG9cIMSaXU",
+  authDomain: "rocketgram-4b663.firebaseapp.com",
+  databaseURL:
+    "https://rocketgram-4b663-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "rocketgram-4b663",
+  storageBucket: "rocketgram-4b663.appspot.com",
+  messagingSenderId: "479359383313",
+  appId: "1:479359383313:web:5141b5959e00049c67beb0",
 };
 
 // Initialize Firebase
