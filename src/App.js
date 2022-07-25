730720d8d0e0543299766de23dae3b940e187e5d
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-07-25 17:44:42 +0800

    added blank text box input field

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
     // When Firebase changes, update local state, which will update local UI
     this.state = {
       messages: [],
+      textBlankInput: "",
     };
   }
 
