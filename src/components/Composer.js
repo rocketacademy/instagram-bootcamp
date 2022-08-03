3d29e36beada94b449d47d130f25864aeeb6f440
adamvader <adamonglt@hotmail.com>
2022-08-03 22:37:45 +0800

    refactored to hooks for composer plus renaming of variables

--- src/components/Composer.js
+++ src/components/Composer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { push, ref as databaseRef, set } from "firebase/database";
 import {
   getDownloadURL,
@@ -11,89 +11,156 @@ import "./App.css";
 const POSTS_FOLDER_NAME = "posts";
 const IMAGES_FOLDER_NAME = "images";
 
-class Composer extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
-    // When Firebase changes, update local state, which will update local UI
-    this.state = {
-      inputTextValue: "",
-      fileInputFile: null,
-      fileInputValue: "",
-    };
-  }
+const Composer = (props) => {
+  const [caption, setCaption] = useState("");
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileName, setImageFileName] = useState("");
 
-  // **insert handleChange for the input fileupload here**
-  handleFileInputChange = (e) => {
-    this.setState({
-      fileInputFile: e.target.files[0],
-      fileInputValue: e.target.value,
-    });
+  const handleInputChange = (e) => {
+    if (e.target.name === "caption") {
+      setCaption(e.target.value);
+    } else if (e.target.name === "imageFileName") {
+      setImageFile(e.target.files[0]);
+      setImageFileName(e.target.value);
+    }
   };
 
-  // **insert handleChange for the input textfield here**
-  handleTextInputChange = (e) => {
-    this.setState({
-      inputTextValue: e.target.value,
-    });
-  };
-
-  // **insert handleSubmit here for what happens when click send**
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     const fileRef = storageRef(
       storage,
-      `${IMAGES_FOLDER_NAME}/${this.state.fileInputFile.name}`
+      `${IMAGES_FOLDER_NAME}/${imageFile.name}`
     );
 
-    uploadBytes(fileRef, this.state.fileInputFile).then(() => {
+    uploadBytes(fileRef, imageFile).then(() => {
       getDownloadURL(fileRef).then((downloadUrl) => {
-        const messagesListRef = databaseRef(database, POSTS_FOLDER_NAME);
-        const newMessagesRef = push(messagesListRef);
-        set(newMessagesRef, {
+        const postsListRef = databaseRef(database, POSTS_FOLDER_NAME);
+        const newPostRef = push(postsListRef);
+        set(newPostRef, {
           imageLink: downloadUrl,
-          text: this.state.inputTextValue,
-          authorEmail: this.props.loggedInUser.email,
+          caption: caption,
+          authorEmail: props.loggedInUser.email,
         });
-        this.setState({
-          fileInputFile: null,
-          fileInputValue: "",
-          inputTextValue: "",
-        });
+        setImageFile(null);
+        setImageFileName("");
+        setCaption("");
       });
     });
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>{this.props.loggedInUser ? this.props.loggedInUser.email : null}</p>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>{props.loggedInUser ? props.loggedInUser.email : null}</p>
         <label>
           Upload:
           <input
             type="file"
-            value={this.state.fileInputValue}
-            onChange={this.handleFileInputChange}
+            name="imageFileName"
+            value={imageFileName}
+            onChange={handleInputChange}
           />
         </label>
         <br />
         <label>
-          Title:
+          Caption:
           <input
             type="text"
-            value={this.state.inputTextValue}
-            onChange={this.handleTextInputChange}
+            name="caption"
+            value={caption}
+            onChange={handleInputChange}
           />
         </label>
-        <input
-          type="submit"
-          value="Upload!"
-          disabled={!this.state.inputTextValue}
-        />
+        <input type="submit" value="Upload!" disabled={!caption} />
       </form>
-    );
-  }
-}
+    </div>
+  );
+};
+
+// class Composer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // Initialise empty messages array in state to keep local state in sync with Firebase
+//     // When Firebase changes, update local state, which will update local UI
+//     this.state = {
+//       inputTextValue: "",
+//       fileInputFile: null,
+//       fileInputValue: "",
+//     };
+//   }
+
+//   // **insert handleChange for the input fileupload here**
+//   handleFileInputChange = (e) => {
+//     this.setState({
+//       fileInputFile: e.target.files[0],
+//       fileInputValue: e.target.value,
+//     });
+//   };
+
+//   // **insert handleChange for the input textfield here**
+//   handleTextInputChange = (e) => {
+//     this.setState({
+//       inputTextValue: e.target.value,
+//     });
+//   };
+
+//   // **insert handleSubmit here for what happens when click send**
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     const fileRef = storageRef(
+//       storage,
+//       `${IMAGES_FOLDER_NAME}/${this.state.fileInputFile.name}`
+//     );
+
+//     uploadBytes(fileRef, this.state.fileInputFile).then(() => {
+//       getDownloadURL(fileRef).then((downloadUrl) => {
+//         const messagesListRef = databaseRef(database, POSTS_FOLDER_NAME);
+//         const newMessagesRef = push(messagesListRef);
+//         set(newMessagesRef, {
+//           imageLink: downloadUrl,
+//           text: this.state.inputTextValue,
+//           authorEmail: this.props.loggedInUser.email,
+//         });
+//         this.setState({
+//           fileInputFile: null,
+//           fileInputValue: "",
+//           inputTextValue: "",
+//         });
+//       });
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <p>{this.props.loggedInUser ? this.props.loggedInUser.email : null}</p>
+//         <label>
+//           Upload:
+//           <input
+//             type="file"
+//             value={this.state.fileInputValue}
+//             onChange={this.handleFileInputChange}
+//           />
+//         </label>
+//         <br />
+//         <label>
+//           Title:
+//           <input
+//             type="text"
+//             value={this.state.inputTextValue}
+//             onChange={this.handleTextInputChange}
+//           />
+//         </label>
+//         <input
+//           type="submit"
+//           value="Upload!"
+//           disabled={!this.state.inputTextValue}
+//         />
+//       </form>
+//     );
+//   }
+// }
 
 export default Composer;
--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -21,7 +21,7 @@ const NewsFeed = () => {
           key: data.key,
           image: value.imageLink,
           author: value.authorEmail,
-          caption: value.text,
+          caption: value.caption,
         },
       ]);
     });
@@ -30,7 +30,7 @@ const NewsFeed = () => {
   let postCards = posts.map((post) => (
     <Card key={post.key} className="Card">
       <Card.Img variant="top" src={post.image} className="Card-Img" />
-      <Card.Text>Title: {post.caption}</Card.Text>
+      <Card.Text>Caption: {post.caption}</Card.Text>
       <Card.Text>Author: {post.author}</Card.Text>
     </Card>
   ));
