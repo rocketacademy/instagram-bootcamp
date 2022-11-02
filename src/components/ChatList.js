83700d91cb048d4991c7300e5a8928150ef58d23
longpaladin <frostarcher7@gmail.com>
2022-11-02 11:11:31 +0800

    basic chat working

---
+++ src/components/ChatList.js
@@ -0,0 +1,44 @@
+import {format } from 'date-fns';
+import {
+  onValue,
+  ref,
+} from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { database } from "../firebase";
+
+export function ChatList() {
+  const [messages,setMessages] = useState([]);
+
+  useEffect(() => {
+
+    const messagesRef = ref(database, "messages");
+    onValue(messagesRef, (snapshot) => {
+      const newMessages = [];
+      snapshot.forEach((childSnapshot) => {
+        newMessages.push(childSnapshot.val());
+
+      });
+      setMessages(newMessages);
+    });
+  },[]);
+
+  return (
+    <div style={{ border: "2px solid white", width: "90%" }}>
+      <h3>Chat List</h3>
+      {messages.map((message, index) => (
+        <div key={index} style={{ border: "2px solid white" }}>
+          <p style={{ fontSize: "12px" }}>
+            {message.name} @ &nbsp;
+            {/* {new Date(message.timestamp).getDate()}/
+            {new Date(message.timestamp).getMonth() + 1}/
+            {new Date(message.timestamp).getFullYear()},{" "}
+            {new Date(message.timestamp).getHours()}
+            {new Date(message.timestamp).getMinutes()} hrs */}
+            {format(new Date(message.timestamp), "PPpp")}
+          </p>
+          <p style={{ fontSize: "12px" }}>{message.chat}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,50 @@
+import { v4 as uuidv4 } from "uuid";
+import {
+  onValue,
+  onChildAdded,
+  orderByChild,
+  orderByValue,
+  orderByKey,
+  query,
+  ref,
+  remove,
+  set,
+  get,
+} from "firebase/database";
+import { getDatabase } from "firebase/database";
+import {getStorage} from "firebase/storage";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+export const storage = getStorage();
 
-// Get a reference to the database service and export the reference for other modules
-export const database = getDatabase(firebaseApp);
+export function getAllMessagesRef(...constraints) {
+  return query(ref(database, "messages"), ...constraints);
+}
+
+export function deleteUser(id) {
+  return remove(ref(database, `users/${id}`));
+}
+
+export function createMessageLog(info) {
+  return set(ref(database, `messages/${uuidv4()}`), info);
+}
+
+export function createMessagesListener(callback = () => null) {
+  return onValue(getAllMessagesRef(orderByChild("timestamp")), callback); 
+}
