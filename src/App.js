c712f3c40b337e795f429814ab52d243e089a8ac
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-28 12:46:10 +0800

    Render messages from user input

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { onChildAdded, push, ref, set } from "firebase/database";
 import { database } from "./firebase";
 import logo from "./logo.png";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     // Initialise empty messages array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
       messages: [],
+      message: "",
     };
   }
 
@@ -33,7 +36,13 @@ class App extends React.Component {
   writeData = () => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    set(newMessageRef, this.state.message);
+    this.setState({ message: "" });
+  };
+
+  handleChange = (e) => {
+    let { name, value } = e.target;
+    this.setState({ [name]: value });
   };
 
   render() {
@@ -48,13 +57,20 @@ class App extends React.Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
+          <Form onSubmit={this.writeData}>
+            <Form.Control
+              name="message"
+              placeholder="Write your message here!"
+              value={this.state.message}
+              onChange={this.handleChange}
+            />
+            <Button variant="light" type="submit">
+              Send
+            </Button>
+          </Form>
           <ol>{messageListItems}</ol>
         </header>
       </div>
     );
   }
 }
-
-export default App;
