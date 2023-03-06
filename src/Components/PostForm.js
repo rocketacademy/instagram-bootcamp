071b85480f0c89b9de3709aa15ea8fc0fa42564b
peanutyabing <sherry.liuyabing@gmail.com>
2023-03-06 14:58:21 +0800

    Rewrite PostForm into a functional component

--- src/Components/PostForm.js
+++ src/Components/PostForm.js
@@ -1,39 +1,36 @@
-import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import sendMe from "../send-me.png";
 
-export default class PostForm extends React.Component {
-  render() {
-    return (
-      <Card key="post-form">
-        <Card.Img
-          variant="top"
-          key={"send-me"}
-          src={sendMe}
-          alt={"Send me a Rocketgram"}
+export default function PostForm(props) {
+  return (
+    <Card key="post-form">
+      <Card.Img
+        variant="top"
+        key={"send-me"}
+        src={sendMe}
+        alt={"Send me a Rocketgram"}
+      />
+      <Form className="post-form" onSubmit={props.handleSubmit}>
+        <Form.Control
+          type="file"
+          ref={props.inputRef}
+          onChange={props.handleFileChange}
         />
-        <Form className="post-form" onSubmit={this.props.handleSubmit}>
-          <Form.Control
-            type="file"
-            ref={this.props.inputRef}
-            onChange={this.props.handleFileChange}
-          />
-          <Form.Control
-            name="message"
-            id="message"
-            as="textarea"
-            rows={2}
-            placeholder="Write your message here!"
-            value={this.props.message}
-            onChange={this.props.handleTextChange}
-          />
-          <Button variant="primary" type="submit">
-            Post
-          </Button>
-        </Form>
-      </Card>
-    );
-  }
+        <Form.Control
+          name="message"
+          id="message"
+          as="textarea"
+          rows={2}
+          placeholder="Write your message here!"
+          value={props.message}
+          onChange={props.handleTextChange}
+        />
+        <Button variant="primary" type="submit">
+          Post
+        </Button>
+      </Form>
+    </Card>
+  );
 }
