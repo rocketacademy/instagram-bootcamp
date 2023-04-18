63318f537a5665c988fa7c42972b32ec1fce654d
markcwy-ra <rocket@markcwy.com>
2023-04-18 15:51:58 +0800

    Added formatted date

--- src/Components/MessageBubble.js
+++ src/Components/MessageBubble.js
@@ -1,9 +1,29 @@
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 const MessageBubble = (props) => {
   const date = new Date(JSON.parse(props.children.val.date));
+  const ampm = date.getHours() > 12 ? "PM" : "AM";
+  let hour;
+  if (date.getHours() > 12) {
+    hour = date.getHours() - 12;
+  } else if (date.getHours() === 0) {
+    hour = 12;
+  }
+  const displayDate = `${
+    days[date.getDay()]
+  } ${hour}:${date.getMinutes()} ${ampm}`;
   return (
     <li className="message">
       <p className="message-bubble">{props.children.val.content}</p>
-      <p className="message-meta">{date.toLocaleTimeString()}</p>
+      <p className="message-meta">{displayDate}</p>
     </li>
   );
 };
