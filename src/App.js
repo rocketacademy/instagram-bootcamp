9ce3351ad70d487dd748a33e8c0c0cde17b6ae21
prettykaur <pkaur.dev@gmail.com>
2023-04-19 16:38:19 +0800

    Add timestamp and file upload functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { database, storage } from "./firebase";
+import {
+  ref as storeRef,
+  getDownloadURL,
+  uploadBytesResumable,
+} from "firebase/storage";
 import logo from "./logo.png";
 import "./App.css";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+const STORE_FILES_KEY = "files";
 
 class App extends React.Component {
   constructor(props) {
@@ -15,6 +21,7 @@ class App extends React.Component {
     this.state = {
       messages: [],
       messageInput: "",
+      fileInput: null,
     };
   }
 
@@ -48,16 +55,53 @@ class App extends React.Component {
     e.preventDefault();
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, this.state.messageInput);
+    const fileRef = storeRef(
+      storage,
+      `${STORE_FILES_KEY}/${this.state.fileInput.name}`
+    );
+
+    // Trying to add date (unsuccessfully)
+    // const newMessageRef = push(messageListRef);
+    // console.log("newMessageRef: ", newMessageRef);
+    const timestamp = new Date();
+    console.log(timestamp.toLocaleString("en-GB").slice(0, -3));
+
+    uploadBytesResumable(fileRef, this.state.fileInput).then(() => {
+      getDownloadURL(fileRef).then((url) => {
+        const newMessage = {
+          message: this.state.messageInput,
+          timestamp: timestamp.toLocaleString("en-GB").slice(0, -3),
+          file: url,
+        };
+        set(newMessageRef, newMessage).then(() => {
+          this.setState({ messageInput: "", fileInput: null });
+        });
+      });
+    });
+
+    // push(messageListRef, newMessage);
+  };
+
+  handleFileChange = (e) => {
     this.setState({
-      messageInput: "",
+      fileInput: e.target.files[0],
     });
   };
 
   render() {
     // Convert messages in state to message JSX elements to render
     let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val}</li>
+      <div>
+        <li key={message.key}>
+          {message.val.message} {message.val.timestamp}
+        </li>
+        <img
+          src={message.val.file}
+          alt="beautiful sunset"
+          width="50vw"
+          height="30vh"
+        />
+      </div>
     ));
     return (
       <div className="App">
@@ -75,6 +119,13 @@ class App extends React.Component {
               onChange={this.handleChange}
             />
             <input type="submit" value="➤" />
+            <br />
+            <input
+              id="fileInput"
+              type="file"
+              onChange={this.handleFileChange}
+            />
+
             {/* <button onClick={this.writeData}>Send</button> */}
           </form>
           <ol>{messageListItems}</ol>
