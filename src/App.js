68f76981c1835280107154524cfcad17f2fd15ce
dexterch91 <dexterchewxh@hotmail.sg>
2023-05-18 01:42:54 +0800

    initial submission for the server chat room

--- src/App.js
+++ src/App.js
@@ -14,14 +14,23 @@ class App extends React.Component {
     // When Firebase changes, update local state, which will update local UI
     this.state = {
       messages: [],
+      disableState: true,
+      //Separate dictionary for the formData
+      formData: {
+        name: '',
+        chat: '',
+      },
     };
+
   }
 
   componentDidMount() {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
     // onChildAdded will return data for every child at the reference and every subsequent new child
+    // onChildAdded: Pulls data from the server into local 
     onChildAdded(messagesRef, (data) => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
+      console.log(data)
       this.setState((state) => ({
         // Store message key so we can use it as a key in our list items when rendering messages
         messages: [...state.messages, { key: data.key, val: data.val() }],
@@ -30,27 +39,108 @@ class App extends React.Component {
   }
 
   // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
+  // Writing data into the server
+  writeData = (textInput) => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    set(newMessageRef, textInput);
+    // console.log(messageListRef);
+    // console.log(newMessageRef);
   };
 
+  MessageDateTime = () => {
+    const messageDate = new Date();
+    const formattedDate = messageDate.toLocaleDateString();
+    const formattedTime = messageDate.toLocaleTimeString();
+    return ("["+formattedDate + " "+ formattedTime +"]")
+  }
+
+  //When there is an input change
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    let nameCheck = this.state.formData.name.length > 0 ? true : false;
+    let msgCheck = this.state.formData.chat.length > 0 ? true : false;
+    console.log(nameCheck,msgCheck)
+    this.setState((prevState) => ({ //updates the state
+      disableState: nameCheck && msgCheck ? false : true,
+      formData: {
+        ...prevState.formData,
+        [name]: value,
+      },
+    }));
+  };
+
+  //When there is an input change
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform the desired action with the form data
+    console.log('Form submitted!');
+    console.log('Name:', this.state.formData.name);
+    console.log('Message:', this.state.formData.chat);
+    let chatLog = this.MessageDateTime() + " " + this.state.formData.name + ": " +this.state.formData.chat;
+    this.writeData(chatLog) //writes into the data
+    // Reset the form fields
+    this.setState({
+      formData: {
+        name: '',
+        chat: '',
+      },
+    });
+  };
+
+
+
   render() {
+    const { name, chat } = this.state.formData;
+
     // Convert messages in state to message JSX elements to render
     let messageListItems = this.state.messages.map((message) => (
       <li key={message.key}>{message.val}</li>
     ));
+
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Messaging Application</h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
-          <ol>{messageListItems}</ol>
+
+          <h3>Chat History</h3>
+          <div className="messageHist">
+          <ol className="invertedList">
+            {messageListItems}
+          </ol>
+          </div>
+        
+        <form onSubmit={this.handleSubmit}>
+          <p>Join the conversation:</p>
+        
+        <div className="chatInput">
+        <label className="chatPax">
+          <p className="formLabel">Name:</p>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleInputChange}
+          />
+        </label>
+
+        <label className="chatPax">
+          <p className="formLabel">Chat:</p>
+          <input
+            type="text"
+            name="chat"
+            value={chat}
+            onChange={this.handleInputChange}
+          />
+        </label>
+
+        <button type="submit" disabled = {this.state.disableState}>Submit</button>
+       
+        </div>
+        </form>
+
         </header>
       </div>
     );
