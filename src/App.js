137e005d3c257a85b36340fcdb576a0832245bb8
alaynelxr <alaynelxr@gmail.com>
2023-05-23 19:42:27 +0800

    added upload file and render uploaded files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { database, storage } from "./firebase";
 import logo from "./logo.png";
 import "./App.css";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+const STORAGE_KEY = "images/";
 
 class App extends React.Component {
   constructor(props) {
@@ -14,6 +20,11 @@ class App extends React.Component {
     // When Firebase changes, update local state, which will update local UI
     this.state = {
       messages: [],
+      textInputValue: "",
+      date: "",
+      time: "",
+      fileInputFile: null,
+      fileInputValue: "",
     };
   }
 
@@ -29,27 +40,92 @@ class App extends React.Component {
     });
   }
 
-  // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
+  handleChange = (event) => {
+    this.setState({ textInputValue: event.target.value });
+  };
+
+  writeData = (url) => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    set(newMessageRef, {
+      message: this.state.textInputValue,
+      date: new Date().toLocaleDateString(),
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      url: url,
+    });
+    this.setState({
+      message: "",
+      date: "",
+      time: "",
+      fileInputFile: null,
+      fileInputValue: "",
+    });
+  };
+
+  // Note use of array fields syntax to avoid having to manually bind this method to the class
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const fullStorageRef = storageRef(
+      storage,
+      STORAGE_KEY + this.state.fileInputFile.name
+    );
+
+    uploadBytes(fullStorageRef, this.state.fileInputFile).then((snapshot) => {
+      getDownloadURL(fullStorageRef, this.state.fileInputFile.name).then(
+        (url) => {
+          this.writeData(url);
+        }
+      );
+    });
+
+    // Reset input field after submit
+    this.setState({ textInputValue: "" });
   };
 
   render() {
     // Convert messages in state to message JSX elements to render
     let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val}</li>
+      <li key={message.key}>
+        Message: {message.val.message} <br />
+        Date:{message.val.date} - {message.val.time}
+        {message.val.url ? (
+          <img src={message.val.url} alt={message.val.message} />
+        ) : (
+          <p>No images</p>
+        )}
+      </li>
     ));
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              value={this.state.textInputValue}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="Send"
+              // Disable Send button when text input is empty
+              disabled={!this.state.textInputValue}
+            />
+            <input
+              type="file"
+              name="file"
+              value={this.state.fileInputValue}
+              onChange={(e) => {
+                this.setState({
+                  fileInputFile: e.target.files[0],
+                  fileInputValue: e.target.file,
+                });
+              }}
+            />
+          </form>
           <ol>{messageListItems}</ol>
         </header>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
+  apiKey: "AIzaSyBwzx_52za3BM109NgVuJomKdrDF8Lnbrs",
+  authDomain: "rocketgram-98b44.firebaseapp.com",
   // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  databaseURL:
+    "https://rocketgram-98b44-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "rocketgram-98b44",
+  storageBucket: "rocketgram-98b44.appspot.com",
+  messagingSenderId: "572134929616",
+  appId: "1:572134929616:web:27f15b7e09e047a49e9ad5",
 };
 
 // Initialize Firebase
@@ -19,3 +21,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 // Get a reference to the database service and export the reference for other modules
 export const database = getDatabase(firebaseApp);
+export const storage = getStorage(firebaseApp);
