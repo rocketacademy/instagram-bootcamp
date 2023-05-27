bbc2b7c008e071ad1ef8c5a2fa96c81e8706bb45
khloeli <chloelihuimin@gmail.com>
2023-05-27 08:16:06 +0800

    add database and storage for images

--- src/App.js
+++ src/App.js
@@ -1,56 +1,135 @@
 import React from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
-import logo from "./logo.png";
+import { database, storage } from "./firebase";
 import "./App.css";
 
-// Save the Firebase message folder name as a constant to avoid bugs due to misspelling
-const DB_MESSAGES_KEY = "messages";
+// import to use methods to access and upload from firebase/storage
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+
+// Save the Firebase post folder name as a constant to avoid bugs due to misspelling
+const DB_POST_FOLDER_NAME = "post";
+const IMAGE_FOLDER_NAME = "images";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
+    // Initialise empty posts array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
-      messages: [],
+      currPost: "",
+      posts: [],
+      date: "",
+      time: "",
+      fileInputFile: null,
+      fileInputValue: "",
     };
   }
 
   componentDidMount() {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
+    const postRef = ref(database, DB_POST_FOLDER_NAME);
     // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
+    onChildAdded(postRef, (data) => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
       this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
+        // Store post key so we can use it as a key in our list items when rendering posts
+        posts: [...state.posts, { key: data.key, val: data.val() }],
       }));
     });
   }
 
   // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+  writeData = (url) => {
+    const postRef = ref(database, DB_POST_FOLDER_NAME);
+    const newPostRef = push(postRef);
+    set(newPostRef, {
+      post: this.state.currPost,
+      date: new Date().toLocaleDateString(),
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      url: url,
+    });
+    this.setState({
+      currPost: "",
+      date: "",
+      time: "",
+      fileInputFile: null,
+      fileInputValue: "",
+    });
+  };
+
+  // Note use of array fields syntax to avoid having to manually bind this method to the class
+  handleSubmit = (e) => {
+    // Prevent default form submit behaviour that will reload the page
+    e.preventDefault();
+
+    // Store images in an images folder in Firebase Storage
+    const fileRef = storageRef(
+      storage,
+      ` ${IMAGE_FOLDER_NAME}/${this.state.fileInputFile.name}`
+    );
+    console.log(` ${IMAGE_FOLDER_NAME}/${this.state.fileInputFile.name}`);
+
+    uploadBytes(fileRef, this.state.fileInputFile).then((snapshot) => {
+      getDownloadURL(fileRef).then((url) => {
+        this.writeData(url);
+      });
+    });
+  };
+
+  // Note use of array fields syntax to avoid having to manually bind this method to the class
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
-    // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val}</li>
+    // Convert posts in state to post JSX elements to render
+    let postListItems = this.state.posts.map((post) => (
+      <p>
+        {post.val.url ? (
+          <img src={post.val.url} alt={post.val.name} />
+        ) : (
+          <p>No image</p>
+        )}{" "}
+        <br />
+        {post.val.post} <br />
+        {post.val.date} - {post.val.time} <br />
+      </p>
     ));
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
-          <ol>{messageListItems}</ol>
+          <p>{postListItems}</p>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="currPost"
+              value={this.state.currPost}
+              placeholder="Input your post"
+              onChange={(e) => this.handleChange(e)}
+            />
+            <br />
+            <input
+              type="file"
+              name="file"
+              value={this.state.fileInputValue}
+              onChange={(e) => {
+                this.setState({
+                  fileInputFile: e.target.files[0],
+                  fileInputValue: e.target.value,
+                });
+              }}
+            />
+            <input type="submit" name="submit" />
+          </form>
         </header>
       </div>
     );
