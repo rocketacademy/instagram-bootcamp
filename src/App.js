1663a0f346c054c2cc045716a4857d07f3be2062
ryanlerler <lyp2726@outlook.com>
2023-06-03 16:24:29 +0800

    Add file upload & refactor to posts

--- src/App.js
+++ src/App.js
@@ -1,79 +1,165 @@
 import React from "react";
-import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { onChildAdded, push, ref as databaseRef, set } from "firebase/database";
+import { database, storage } from "./firebase";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 import logo from "./logo.png";
 import "./App.css";
+import Card from "react-bootstrap/Card";
+import { Button } from "react-bootstrap";
 
-// Save the Firebase message folder name as a constant to avoid bugs due to misspelling
-const DB_MESSAGES_KEY = "messages";
+// Save the Firebase post folder name as a constant to avoid bugs due to misspelling
+const POST_KEY = "posts";
+const STORAGE_KEY = "images/";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
+    // Initialise empty posts array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
-      messages: [],
-      input: "",
+      posts: [],
+      textInput: "",
+      fileInputFile: null,
+      fileInputValue: "",
+      likeClicked: false,
+      likeCount: 0,
     };
   }
 
   componentDidMount() {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
+    const postRef = databaseRef(database, POST_KEY);
     // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
+    onChildAdded(postRef, (data) => {
       console.log(data);
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
       this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
+        // Store posts key so we can use it as a key in our card items when rendering posts
+        posts: [...state.posts, { key: data.key, val: data.val() }],
       }));
     });
   }
 
   // Note use of arrow fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
-    set(newMessageRef, {
-      input: this.state.input,
+  writeData = (url) => {
+    const postRef = databaseRef(database, POST_KEY);
+    const newPostRef = push(postRef);
+    set(newPostRef, {
+      textInput: this.state.textInput,
       date: new Date().toLocaleString(),
+      url: url,
+      likeCount: this.state.likeCount,
     });
 
     this.setState({
-      input: "",
+      textInput: "",
+      fileInputFile: null,
+      fileInputValue: "",
     });
   };
 
-  handleChange = (e) => {
+  handleTextInputChange = (e) => {
     const { name, value } = e.target;
     this.setState({
       [name]: value,
     });
   };
 
+  handleFileInputChange = (e) => {
+    console.log(e);
+    const { files, value } = e.target;
+    this.setState({
+      fileInputFile: files[0],
+      fileInputValue: value,
+    });
+  };
+
+  submit = (e) => {
+    e.preventDefault();
+
+    const fileStorageRef = storageRef(
+      storage,
+      STORAGE_KEY + this.state.fileInputFile.name
+    );
+
+    uploadBytes(fileStorageRef, this.state.fileInputFile)
+      .then(() => getDownloadURL(fileStorageRef))
+      .then((url) => this.writeData(url));
+  };
+
+  handleLikeCount = () => {
+    this.setState((state) => ({
+      likeCount: state.likeClicked ? state.likeCount + 1 : state.likeCount - 1,
+    }));
+  };
+
+  handleLikeButton = () => {
+    this.setState(
+      (state) => ({
+        likeClicked: !state.likeClicked,
+      }),
+      () => this.handleLikeCount()
+    );
+  };
+
+  componentDidUpdate() {
+    console.log(this.state.likeClicked);
+    console.log(this.state.likeCount);
+  }
+
   render() {
-    console.log(this.state.messages);
-    // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>
-        {message.val.input} -- {message.val.date}
-      </li>
+    console.log(this.state.posts);
+    console.log(this.state.fileInputFile);
+    console.log(this.state.fileInputValue);
+    // Convert posts in state to JSX elements to render
+    let postItems = this.state.posts.map((post) => (
+      <Card key={post.key}>
+        {post.val.url ? (
+          <>
+            <Card.Img variant="top" src={post.val.url} className="Card-img" />{" "}
+            <Button variant="white" onClick={this.handleLikeButton}>
+              ❤ {post.val.likeCount}
+            </Button>
+          </>
+        ) : (
+          <p>No images</p>
+        )}
+
+        <Card.Body>
+          <Card.Title>{post.val.date}</Card.Title>
+          <Card.Text>{post.val.textInput} </Card.Text>
+        </Card.Body>
+      </Card>
     ));
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           {/* TODO: Add input field and add text input as messages in Firebase */}
-          <label>Input your message below: </label>
-          <input
-            type="text"
-            name="input"
-            value={this.state.input}
-            onChange={this.handleChange}
-          />
-          <button onClick={this.writeData}>Send</button>
-          <ol>{messageListItems}</ol>
+          <form onSubmit={this.submit}>
+            <label>Submit your thoughts below: </label>
+            <br />
+            <input
+              type="text"
+              name="textInput"
+              value={this.state.textInput}
+              onChange={this.handleTextInputChange}
+            />
+            <br />
+            <input
+              type="file"
+              name="file"
+              value={this.state.fileInputValue}
+              onChange={this.handleFileInputChange}
+            />
+            <br />
+            <button>Send</button>
+          </form>
+          <ol>{postItems.reverse()}</ol>
         </header>
       </div>
     );
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
@@ -20,3 +21,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 // Get a reference to the database service and export the reference for other modules
 export const database = getDatabase(firebaseApp);
+export const storage = getStorage(firebaseApp);
