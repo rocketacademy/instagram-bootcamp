31af7cbb02e31853b83c0e1ca428cb98ec478ff6
khloeli <chloelihuimin@gmail.com>
2023-06-04 11:03:46 +0800

    change to functional code and add authform

--- src/App.js
+++ src/App.js
@@ -2,22 +2,53 @@ import React from "react";
 import "./App.css";
 import Composer from "./Component/Composer";
 import NewsFeed from "./Component/NewsFeed";
+import AuthForm from "./Component/AuthForm";
+import { useState, useEffect } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div>
-            {" "}
-            <NewsFeed />
-            <br />
-            <Composer />
-          </div>
-        </header>
-      </div>
-    );
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        setUser(user);
+        setUsername(capitalizeFirstLetter(user.email.split("@")[0]));
+      }
+    });
+  }, []);
+  console.log(user);
+  console.log(username);
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          {isLoggedIn ? <h2>Welcome {username}</h2> : null}
+          {isLoggedIn ? (
+            <button
+              onClick={(e) => {
+                setIsLoggedIn(false);
+                signOut(auth);
+                setUser({});
+              }}
+            >
+              SignOut
+            </button>
+          ) : null}
+          {isLoggedIn ? <Composer username={username} /> : <AuthForm />}
+          <br /> <NewsFeed />
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/Component/NewsFeed.js
+++ src/Component/NewsFeed.js
@@ -1,46 +1,35 @@
 import React from "react";
 import { onChildAdded, ref } from "firebase/database";
 import { database } from "../firebase";
+import { useState, useEffect } from "react";
 
 // Save the Firebase post folder name as a constant to avoid bugs due to misspelling
-const DB_POST_FOLDER_NAME = "post";
+const DB_POST_FOLDER_NAME = "posts";
 
-export default class NewsFeed extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise empty posts array in state to keep local state in sync with Firebase
-    // When Firebase changes, update local state, which will update local UI
-    this.state = {
-      posts: [],
-    };
-  }
+export default function NewsFeed() {
+  const [posts, setPosts] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     const postRef = ref(database, DB_POST_FOLDER_NAME);
     // onChildAdded will return data for every child at the reference and every subsequent new child
     onChildAdded(postRef, (data) => {
-      // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      this.setState((state) => ({
-        // Store post key so we can use it as a key in our list items when rendering posts
-        posts: [...state.posts, { key: data.key, val: data.val() }],
-      }));
+      setPosts((state) => [...state, { key: data.key, val: data.val() }]);
     });
-  }
+  }, []);
+  // The empty array indicates that the useEffect doesn’t have any dependencies on any state variables. Therefore, the callback function is only called once the page renders in this case.
 
-  render() {
-    // Convert posts in state to post JSX elements to render
-    let postListItems = this.state.posts.map((post) => (
-      <p>
-        {post.val.url ? (
-          <img src={post.val.url} alt={post.val.name} />
-        ) : (
-          <p>No image</p>
-        )}{" "}
-        <br />
-        {post.val.post} <br />
-        {post.val.date} - {post.val.time} <br />
-      </p>
-    ));
-    return <p>{postListItems}</p>;
-  }
+  // Convert posts in state to post JSX elements to render
+  let postListItems = posts.map((post) => (
+    <div key={post.key}>
+      {post.val.url ? (
+        <img src={post.val.url} alt={post.val.name} />
+      ) : (
+        <p>No image</p>
+      )}{" "}
+      <br />
+      {post.val.posts} <br />
+      {post.val.author}:{post.val.date} - {post.val.time} <br />
+    </div>
+  ));
+  return <div>{postListItems}</div>;
 }
