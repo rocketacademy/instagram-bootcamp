2b002b265af9eb1980d882241c3336d0e1f3c92b
calebcianc <calebcc.castro@gmail.com>
2023-06-06 11:08:21 +0800

    Added database, storage, auth, and hosting.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { onChildAdded, push, ref, set, remove } from "firebase/database";
-import { database } from "./firebase";
+import { database, storage } from "./firebase";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Signup from "./Components/Signup";
+import Newsfeed from "./Components/Newsfeed";
+import Composer from "./Components/Composer";
 
-// Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+const STORAGE_KEY = "images";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
-    // When Firebase changes, update local state, which will update local UI
-    this.state = {
-      messages: [],
-      newInput: "",
-    };
-  }
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [newInput, setNewInput] = useState("");
+  const [fileInputFile, setFileInputFile] = useState(null);
+  const [fileInputValue, setFileInputValue] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
-    // onChildAdded will return data for every child at the reference and every subsequent new child
     onChildAdded(messagesRef, (data) => {
-      // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
-      }));
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { key: data.key, val: data.val() },
+      ]);
     });
-  }
+  }, []);
 
-  // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
-    // create a variable for the location where the list of messages are found in the database
+  const writeData = (url) => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
-    // create another variable for the child at the location where the list of messages are found in the database
     const newMessageRef = push(messageListRef);
-    // write data at this child location with the string "abc"
     set(newMessageRef, {
-      text: this.state.newInput,
+      text: newInput,
       date: new Date().toLocaleTimeString(undefined, {
         hour: "numeric",
         minute: "numeric",
       }),
+      url: url,
     });
-    this.setState(() => ({ newInput: "" }));
+    setNewInput("");
+    setFileInputFile(null);
+    setFileInputValue("");
   };
 
-  deleteData = () => {
+  const deleteData = () => {
     const confirmation = window.confirm(
       "Are you sure you want to delete all data?"
     );
@@ -56,7 +56,7 @@ class App extends React.Component {
       remove(messageListRef)
         .then(() => {
           console.log("Data deleted successfully.");
-          this.setState({ messages: [] });
+          setMessages([]);
         })
         .catch((error) => {
           console.error("Error deleting data:", error);
@@ -64,77 +64,44 @@ class App extends React.Component {
     }
   };
 
-  handleChange = (e) => {
-    this.setState(() => ({ newInput: e.target.value }));
+  const submit = () => {
+    const fullStorageRef = storageRef(
+      storage,
+      STORAGE_KEY + "/" + fileInputFile.name
+    );
+    uploadBytes(fullStorageRef, fileInputFile).then((snapshot) => {
+      getDownloadURL(fullStorageRef, fileInputFile.name).then((url) => {
+        writeData(url);
+      });
+    });
   };
 
-  render() {
-    // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
-      <p key={message.key}>
-        {message.val.date}: {message.val.text}
-      </p>
-    ));
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div
-            style={{
-              position: "fixed",
-              bottom: "10vh",
-              width: "50%",
-              display: "flex",
-              textAlign: "left",
-            }}
-          >
-            <ol>{messageListItems}</ol>
-          </div>
-          <div
-            style={{
-              position: "fixed",
-              bottom: "5vh",
-              width: "50%",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <input
-              type="text"
-              class="form-control form-control-lg"
-              placeholder="What's on your mind?"
-              value={this.state.newInput}
-              onChange={(e) => this.handleChange(e)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  e.preventDefault(); // Prevent form submission
-                  this.writeData();
-                }
-              }}
-              style={{ width: "100%" }}
-            />
-            <button
-              type="button"
-              class="btn btn-primary btn-lg"
-              onClick={this.writeData}
-              disabled={!this.state.newInput}
-            >
-              Send
-            </button>
-            <button
-              type="button"
-              class="btn btn-danger btn-lg"
-              onClick={this.deleteData}
-              disabled={this.state.messages.length === 0}
-            >
-              Delete
-            </button>
-          </div>
-        </header>
-      </div>
-    );
-  }
-}
+  const handleChange = (e) => {
+    setNewInput(e.target.value);
+  };
+
+  const imageUploadSetState = (e) => {
+    setFileInputFile(e.target.files[0]);
+    setFileInputValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <Signup />
+
+      <Newsfeed messages={messages} />
+
+      <Composer
+        newInput={newInput}
+        handleChange={handleChange}
+        submit={submit}
+        imageUploadSetState={imageUploadSetState}
+        fileInputValue={fileInputValue}
+        deleteData={deleteData}
+        messages={messages}
+      />
+    </div>
+  );
+};
 
 export default App;
-
-// flow of information goes from the user input > database child location > onChildAdded listener which setStates to update the messages array with the latest message > render () {} then maps out the messages
