c7b353e93b5c895180fd99e1f1a3a4171c80b190
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-06-20 11:15:48 +0800

    example_repo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from "./logo.png";
 import "./App.css";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
-const DB_MESSAGES_KEY = "messages";
+const DB_STUDENTS_KEY = "students";
 
 class App extends React.Component {
   constructor(props) {
@@ -13,33 +13,40 @@ class App extends React.Component {
     // Initialise empty messages array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
-      messages: [],
+      students: [],
+      name: "",
     };
   }
 
   componentDidMount() {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
+    const studentsRef = ref(database, DB_STUDENTS_KEY);
     // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
+    onChildAdded(studentsRef, (data) => {
+      console.log(data);
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
       this.setState((state) => ({
         // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
+        students: [...state.students, { key: data.key, val: data.val() }],
       }));
     });
   }
 
   // Note use of array fields syntax to avoid having to manually bind this method to the class
   writeData = () => {
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    const studentListRef = ref(database, DB_STUDENTS_KEY);
+    const newStudentRef = push(studentListRef);
+    set(newStudentRef, {
+      name: this.state.name,
+      date: new Date().toLocaleString(),
+    });
   };
 
   render() {
     // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val}</li>
+    let studentListItems = this.state.students.map((student) => (
+      <li key={student.key}>
+        {student.val.date}- {student.val.name}
+      </li>
     ));
     return (
       <div className="App">
@@ -48,9 +55,18 @@ class App extends React.Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          {/* TODO: Add input field and add text input as messages in Firebase */}
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(e) => {
+              this.setState({
+                name: e.target.value,
+              });
+            }}
+            placeholder="Add a name here"
+          />
           <button onClick={this.writeData}>Send</button>
-          <ol>{messageListItems}</ol>
+          <ol>{studentListItems}</ol>
         </header>
       </div>
     );
