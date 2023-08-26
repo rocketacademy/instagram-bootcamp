4bc2a9e529a7e5dd2cfcb25eec030d766039cf07
chengwaikoo <chengwai.koo@gmail.com>
2023-08-26 11:40:25 +0800

    feat: move config details to env file

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,12 @@ import { getDatabase } from "firebase/database";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
+  authDomain: "cwkoo-rocketgram.firebaseapp.com",
+  projectId: "cwkoo-rocketgram",
+  storageBucket: "cwkoo-rocketgram.appspot.com",
+  messagingSenderId: `${process.env.REACT_APP_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
 // Initialize Firebase
