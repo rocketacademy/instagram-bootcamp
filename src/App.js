c2d69aed0513c959987477c2182bdd033a596793
llja <lesterlim.ja@gmail.com>
2023-08-26 12:27:31 +0800

    trying to convert into functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
 import { database } from "./firebase";
 import logo from "./logo.png";
@@ -7,38 +7,33 @@ import "./App.css";
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
-    // When Firebase changes, update local state, which will update local UI
-    this.state = {
-      messages: [],
-    };
-  }
-
-  componentDidMount() {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
-    // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
-      // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
-      }));
-    });
-  }
-
+function App () {
+  const [state,setState]=useState({
+    messages:null,
+  })
   // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
+  const writeData = () => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
     set(newMessageRef, "abc");
   };
 
-  render() {
+    useEffect(()=>{
+      const messagesRef = ref(database, DB_MESSAGES_KEY);
+      // onChildAdded will return data for every child at the reference and every subsequent new child
+    
+      onChildAdded(messagesRef, (data) => {
+        // Add the subsequent child to local component state, initialising a new array to trigger re-render
+        setState((state) => ({
+          // Store message key so we can use it as a key in our list items when rendering messages
+          messages: [...state.messages, { key: data.key, val: data.val() }],
+        }));
+      });
+    },[])
+
     // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
+    // The error is here!
+    let messageListItems = state.messages.map((message) => (
       <li key={message.key}>{message.val}</li>
     ));
     return (
@@ -46,15 +41,14 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Rocketgram Chat App
           </p>
           {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
+          <button onClick={()=>writeData}>Send</button>
           <ol>{messageListItems}</ol>
         </header>
       </div>
     );
-  }
 }
 
 export default App;
