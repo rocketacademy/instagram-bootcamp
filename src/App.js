28aae443c87c1d27f9e9362bbf8d9c71accbd260
chengwaikoo <chengwai.koo@gmail.com>
2023-08-29 16:16:22 +0800

    feat: add link to image

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import "./App.css";
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages/";
 const STORAGE_IMAGES_KEY = "images/";
+const IMAGE_FILE_TYPES = ["image/jpeg", "image/png"];
 
 function App() {
   // Initialise empty messages array in state to keep local state in sync with Firebase
@@ -18,13 +19,14 @@ function App() {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   const [file, setFile] = useState(null);
+  const [fileLabel, setFileLabel] = useState("");
 
   const messagesRef = useRef(databaseRef(database, DB_MESSAGES_KEY));
 
   useEffect(() => {
     // onChildAdded will return data for every child at the reference and every subsequent new child
     onChildAdded(messagesRef.current, (data) => {
-      const { message, timestamp } = data.val();
+      const { message, timestamp, url } = data.val();
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
       setMessages((prevMessages) =>
         // Store message key so we can use it as a key in our list items when rendering messages
@@ -34,6 +36,7 @@ function App() {
             key: data.key,
             message,
             timestamp: new Date(timestamp),
+            url,
           },
         ]
       );
@@ -46,7 +49,16 @@ function App() {
 
   const handleFileChange = (event) => {
     const newFile = event.target.files[0];
-    setFile(newFile);
+
+    if (newFile) {
+      if (!IMAGE_FILE_TYPES.includes(newFile.type)) {
+        alert("Invalid file type. Choose a JPG or PNG file.");
+        setFileLabel("");
+        return;
+      }
+
+      setFile(newFile);
+    }
   };
 
   const handleSubmit = (event) => {
@@ -58,23 +70,26 @@ function App() {
     const newFileRef = storageRef(storage, STORAGE_IMAGES_KEY + file.name);
 
     uploadBytes(newFileRef, file)
-      .then((snapshot) => {
-        console.log("Upload complete");
-        getDownloadURL(snapshot.ref);
+      .then((snapshot) => getDownloadURL(snapshot.ref))
+      .then((url) => {
+        set(newMessageRef, { message, timestamp, url });
+        setFileLabel("");
       })
-      .then((url) => set(newMessageRef, { message, timestamp, url }))
       .catch((error) => console.error("Upload error:", error));
   };
 
   // Convert messages in state to message JSX elements to render
-  let messageListItems = messages.map((message) => (
-    <li key={message.key}>
-      {message.message}{" "}
-      <span className="timestamp">
-        {message.timestamp.toLocaleString("en-SG")}
-      </span>
-    </li>
-  ));
+  let messageListItems = messages.map((message) => {
+    console.log(message.url);
+    return (
+      <li key={message.key}>
+        {message.message} <a href={message.url}>image</a>
+        <span className="timestamp">
+          {message.timestamp.toLocaleString("en-SG")}
+        </span>
+      </li>
+    );
+  });
 
   return (
     <div className="App">
@@ -86,7 +101,12 @@ function App() {
             <input type="text" value={message} onChange={handleMessageChange} />
           </p>
           <p>
-            <input type="file" onChange={handleFileChange} />
+            <input
+              type="file"
+              value={fileLabel}
+              onChange={handleFileChange}
+              accept=".jpg,.jpeg,.png"
+            />
           </p>
           <p>
             <button type="submit">Send</button>
