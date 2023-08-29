05ab33db1a19accebfd2e1fdb94da3c08816c46d
chengwaikoo <chengwai.koo@gmail.com>
2023-08-29 17:04:01 +0800

    feat: scale mantine default font sizes

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import { MantineProvider, useMantineTheme } from "@mantine/core";
 import "./index.css";
 import App from "./App";
 
+const fontScale = 1.25;
 const theme = {
   fontSizes: {
-    xs: "0.75rem",
-    sm: "0.875rem",
-    md: "1rem",
-    lg: "1.125rem",
-    xl: "1.25rem",
+    xs: `calc(0.75rem * ${fontScale})`,
+    sm: `calc(0.875rem * ${fontScale})`,
+    md: `calc(1rem * ${fontScale})`,
+    lg: `calc(1.125rem * ${fontScale})`,
+    xl: `calc(1.25rem * ${fontScale})`,
+  },
+  headings: {
+    sizes: { h1: { fontSize: `calc(2.125rem * ${fontScale})` } },
   },
   primaryColor: "teal",
 };
 
 function Root() {
-  const theme = useMantineTheme();
-  console.log(theme.fontFamily);
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
       <App />
