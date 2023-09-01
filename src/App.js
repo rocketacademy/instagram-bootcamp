63070ebf7adacb434ff1f4d8ea4f72cf20a7ca84
Cathy Hui <erashine414@icloud.com>
2023-09-01 13:05:54 +0800

    Updated chat app

--- src/App.js
+++ src/App.js
@@ -1,60 +1,72 @@
-import React from "react";
-import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
-import logo from "./logo.png";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { ref, push, set, onValue } from "firebase/database";
+import { database } from "./firebase";
 
-// Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
-    // When Firebase changes, update local state, which will update local UI
-    this.state = {
-      messages: [],
-    };
-  }
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [textInputValue, setTextInputValue] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
-    // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
-      // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
-      }));
+    onValue(messagesRef, (snapshot) => {
+      const data = snapshot.val();
+      const loadedMessages = data
+        ? Object.keys(data).map((key) => ({ key, val: data[key] }))
+        : [];
+      setMessages(loadedMessages);
     });
-  }
+  }, []);
 
-  // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+  const handleChange = (e) => {
+    setTextInputValue(e.target.value);
   };
 
-  render() {
-    // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val}</li>
-    ));
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
-          <ol>{messageListItems}</ol>
-        </header>
-      </div>
-    );
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const messageListRef = ref(database, DB_MESSAGES_KEY);
+    const newMessageRef = push(messageListRef);
+    set(newMessageRef, {
+      text: textInputValue,
+      timestamp: Date.now(),
+    });
+    setTextInputValue("");
+  };
+
+  const messageListItems = messages.map((message) => (
+    <li className="message-item" key={message.key}>
+      {message.val.text}{" "}
+      <span className="timestamp">
+        {new Date(message.val.timestamp).toLocaleString()}
+      </span>
+    </li>
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RocketChat To Yourself 🙊</h1>
+        <form className="chat-form" onSubmit={handleSubmit}>
+          <input
+            className="chat-input"
+            type="text"
+            value={textInputValue}
+            onChange={handleChange}
+          />
+
+          <input
+            className="chat-send"
+            type="submit"
+            value="Send"
+            disabled={!textInputValue}
+          />
+        </form>
+        <ul className="chat-messages">{messageListItems}</ul>
+      </header>
+    </div>
+  );
 }
 
 export default App;
