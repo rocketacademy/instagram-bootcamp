98a878272823195e5ac629434014a474f2a867ee
jewc <jamescew@gmail.com>
2023-09-01 17:00:07 -0400

    Update to firebase.js access parameters

--- src/firebase.js
+++ src/firebase.js
@@ -14,15 +14,26 @@ import { getDatabase } from "firebase/database";
 //   appId: "APP_ID",
 // };
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   // The value of `databaseURL` depends on the location of the database
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  apiKey: "AIzaSyDFGBNICgeDmg5q6I5dusGDOxvoOrWfu-4",
+  authDomain: "rocketgram-e0ba0.firebaseapp.com",
   // The value of `databaseURL` depends on the location of the database
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  databaseURL: "https://rocketgram-e0ba0-default-rtdb.firebasedatabase.app/",
+  projectId: "rocketgram-e0ba0",
+  storageBucket: "rocketgram-e0ba0.appspot.com",
+  messagingSenderId: "293446546026",
+  appId: "1:293446546026:web:6c8382c81b1153835a1013",
 };
 
 // Initialize Firebase
