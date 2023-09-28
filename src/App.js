9b2c617a56112c409f353a6bff18030288e02a4b
root <root@DESKTOP-DT504RQ>
2023-09-28 13:53:21 +0800

    Add Username,password,button and authentication.

--- src/App.js
+++ src/App.js
@@ -8,25 +8,29 @@ import {
   ref,
   set,
   remove,
+  get,
 } from "firebase/database";
-import { database } from "./firebase";
+import { database, auth } from "./firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  sendPasswordResetEmail as sendPasswordResetEmailFirebase,
+} from "firebase/auth";
 
 const DB_MESSAGES_KEY = "messages";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
-  const [incomingUsername, setIncomingUsername] = useState("");
-  const [outgoingUsername, setOutgoingUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newMessage, setNewMessage] = useState(""); // Define newMessage state
   const [isInputEnabled, setIsInputEnabled] = useState(false);
-
-  useEffect(() => {
-    if (incomingUsername && outgoingUsername) {
-      setIsInputEnabled(true);
-    } else {
-      setIsInputEnabled(false);
-    }
-  }, [incomingUsername, outgoingUsername]);
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showChat, setShowChat] = useState(false); // Control visibility of the chat
+  const [nickname, setNickname] = useState(""); // State for user's nickname
 
   useEffect(() => {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
@@ -63,12 +67,95 @@ const App = () => {
       );
     });
 
+    const authStateChangedListener = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        // Retrieve the user's nickname from the database
+        const userNicknameRef = ref(database, `users/${user.uid}/nickname`);
+        const userNicknameSnapshot = await get(userNicknameRef);
+        if (userNicknameSnapshot.exists()) {
+          setNickname(userNicknameSnapshot.val());
+        }
+
+        setUser(user);
+        setIsLoggedIn(true);
+        setIsInputEnabled(true); // Enable input when user is authenticated
+        setShowChat(true); // Show chat when user is authenticated
+      } else {
+        setUser(null);
+        setIsLoggedIn(false);
+        setIsInputEnabled(false); // Disable input when user is not authenticated
+        setShowChat(false); // Hide chat when user is not authenticated
+      }
+    });
+
     return () => {
       messagesAddedListener();
       messagesChangedListener();
       messagesRemovedListener();
+      authStateChangedListener();
     };
   }, []);
+
+  const handleLogin = async () => {
+    try {
+      // Check if email and password are provided
+      if (!email || !password) {
+        throw new Error("Error: Please type in your username and password.");
+      }
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      // Retrieve the user's nickname from the database
+      const userNicknameRef = ref(database, `users/${user.uid}/nickname`);
+      const userNicknameSnapshot = await get(userNicknameRef);
+      if (userNicknameSnapshot.exists()) {
+        setNickname(userNicknameSnapshot.val());
+      }
+
+      alert("You have signed in.");
+    } catch (error) {
+      alert("Error Wrong Username or Password was typed in.");
+    }
+  };
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      alert("User signed out"); // Change to alert
+    } catch (error) {
+      alert("Error signing out: " + error.message); // Change to alert
+    }
+  };
+
+  const handleSignup = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      // Set the user's nickname in the database
+      const userNicknameRef = ref(database, `users/${user.uid}/nickname`);
+      await set(userNicknameRef, nickname);
+
+      alert("User signed up:", user);
+    } catch (error) {
+      alert("Error signing up:", error.message);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email && password) {
+      writeData();
+    }
+  };
 
   const writeData = () => {
     if (newMessage.trim() !== "") {
@@ -78,29 +165,22 @@ const App = () => {
       set(newMessageRef, {
         message: newMessage,
         datetime,
-        user: incomingUsername,
+        user: email,
       });
       setNewMessage("");
     }
   };
 
   const handleInputChange = (event) => {
-    setNewMessage(event.target.value);
+    setNewMessage(event.target.value); // Update newMessage state
   };
 
-  const handleIncomingUsernameChange = (event) => {
-    setIncomingUsername(event.target.value);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
-  const handleOutgoingUsernameChange = (event) => {
-    setOutgoingUsername(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (incomingUsername && outgoingUsername) {
-      writeData();
-    }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
   const handleDeleteMessage = (messageKey) => {
@@ -109,70 +189,107 @@ const App = () => {
   };
 
   const handleClearLog = () => {
-    // Clear all messages
+    // Clear messages from the local state
     setMessages([]);
+
+    // Clear messages from the Firebase Realtime Database
+    const messageListRef = ref(database, DB_MESSAGES_KEY);
+    remove(messageListRef);
+  };
+
+  const handleResetPassword = async () => {
+    try {
+      await sendPasswordResetEmailFirebase(auth, email);
+      alert("Password reset email sent to: " + email);
+    } catch (error) {
+      alert("Error sending password reset email:", error.message);
+    }
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Instagram Chat Rocket Academy Bootcamp</p>
-
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={incomingUsername}
-            onChange={handleIncomingUsernameChange}
-            placeholder="Enter incoming username"
-          />
-          <br />
-          <input
-            type="text"
-            value={outgoingUsername}
-            onChange={handleOutgoingUsernameChange}
-            placeholder="Enter outgoing username"
-          />
-          <br />
-          <input
-            type="text"
-            value={newMessage}
-            onChange={handleInputChange}
-            placeholder="Enter a message"
-            disabled={!isInputEnabled}
-          />
-          <br />
-          <button type="submit" disabled={!isInputEnabled}>
-            Send
-          </button>
-          <button type="button" onClick={handleClearLog}>
-            Clear Log
-          </button>
-        </form>
-
-        <div className="chat-container">
-          {messages.map((message, index) => (
-            <div
-              key={message.key}
-              className={`message ${index % 2 === 0 ? "incoming" : "outgoing"}`}
+        {isLoggedIn ? (
+          <>
+            <button onClick={handleLogout}>Logout</button>
+            {showChat && (
+              <>
+                <div className="chat-container">
+                  {messages.map((message, index) => (
+                    <div
+                      key={message.key}
+                      className={`message ${
+                        index % 2 === 0 ? "incoming" : "outgoing"
+                      }`}
+                    >
+                      <button
+                        className={`delete-button ${
+                          index % 2 === 0 ? "incoming" : "outgoing"
+                        }`}
+                        onClick={() => handleDeleteMessage(message.key)}
+                      >
+                        X
+                      </button>
+                      <div className="message-content">{message.message}</div>
+                      <div className="message-metadata">
+                        <span className="datetime">{message.datetime}</span>
+                        <span className="user">
+                          {index % 2 === 0 ? nickname || email : password}
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    value={newMessage}
+                    onChange={handleInputChange}
+                    placeholder="Enter a message"
+                    disabled={!isInputEnabled}
+                  />
+                  <button type="submit" disabled={!isInputEnabled}>
+                    Send
+                  </button>
+                  <br />
+                </form>
+                <button onClick={handleClearLog}>Clear Log</button>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <input
+              type="text"
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="Enter Email"
+            />
+            <input
+              type="text"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Enter Password"
+            />
+            <br />
+            <input
+              type="text"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+              placeholder="Enter Nickname"
+            />
+            <br />
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleSignup}>Sign Up</button>
+            <button
+              onClick={handleResetPassword}
+              disabled={!email} // Disable reset password if email is empty
             >
-              <button
-                className={`delete-button ${
-                  index % 2 === 0 ? "incoming" : "outgoing"
-                }`}
-                onClick={() => handleDeleteMessage(message.key)}
-              >
-                X
-              </button>
-              <div className="message-content">{message.message}</div>
-              <div className="message-metadata">
-                <span className="datetime">{message.datetime}</span>
-                <span className="user">
-                  {index % 2 === 0 ? incomingUsername : outgoingUsername}
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
+              Forget Password
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJEC_ID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SEANDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
 
@@ -18,3 +19,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 // Get a reference to the database service and export the reference for other modules
 export const database = getDatabase(firebaseApp);
+export const auth = getAuth(firebaseApp);
