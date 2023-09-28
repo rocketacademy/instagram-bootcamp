4885370b7bca653bb43654e29a9a4cf41acee86c
spenceryang <yangzijian@gmail.com>
2023-09-29 00:26:35 +0800

    basic posts, images, like and comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { database, auth } from "./firebase";
+import { database, auth, storage } from "./firebase";
 import {
   ref,
   onChildAdded,
   onChildRemoved,
   push,
   set,
+  get,
+  update,
 } from "firebase/database";
+import { uploadBytes, getDownloadURL, ref as sRef } from "firebase/storage";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -16,6 +19,21 @@ import {
 } from "firebase/auth";
 import logo from "./logo.png";
 import "./App.css";
+import {
+  Box,
+  Button,
+  Input,
+  List,
+  ListItem,
+  FormControl,
+  FormLabel,
+  Image,
+  Text,
+  VStack,
+  HStack,
+  IconButton,
+} from "@chakra-ui/react";
+import { AddIcon, MinusIcon, CheckCircleIcon } from "@chakra-ui/icons";
 
 const DB_MESSAGES_KEY = "messages";
 
@@ -23,9 +41,13 @@ const App = () => {
   const [messages, setMessages] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [file, setFile] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [postMessage, setPostMessage] = useState("");
   const [postTimestamp, setPostTimestamp] = useState("");
+  const [comments, setComments] = useState({});
+  const [likedMessages, setLikedMessages] = useState([]);
 
   useEffect(() => {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
@@ -71,16 +93,89 @@ const App = () => {
     setEmail("");
   };
 
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      console.log("Signed out successfully");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+
+  const handleLike = async (messageKey) => {
+    const userID = auth.currentUser.uid;
+    const messageRef = ref(database, `${DB_MESSAGES_KEY}/${messageKey}`);
+    const message = (await get(messageRef)).val();
+
+    let messageLikes = message.likes || [];
+
+    if (messageLikes.includes(userID)) {
+      // Unlike
+      messageLikes = messageLikes.filter((id) => id !== userID);
+    } else {
+      // Like
+      messageLikes.push(userID);
+    }
+
+    await update(messageRef, { likes: messageLikes });
+  };
+
+  const handleComment = async (messageKey) => {
+    const comment = comments[messageKey];
+    if (comment) {
+      const messageRef = ref(database, `${DB_MESSAGES_KEY}/${messageKey}`);
+      const message = (await get(messageRef)).val();
+
+      let messageComments = message.comments || [];
+      messageComments.push(comment);
+
+      await update(messageRef, { comments: messageComments });
+      // Update local state
+      setMessages((prevMessages) => {
+        return prevMessages.map((msg) => {
+          if (msg.key === messageKey) {
+            return {
+              ...msg,
+              val: {
+                ...msg.val,
+                comments: messageComments,
+              },
+            };
+          }
+          return msg;
+        });
+      });
+
+      setComments({ ...comments, [messageKey]: "" }); // Clear the comment input
+    }
+  };
+
   const writeData = async (e) => {
     e.preventDefault();
+    const currentTime = new Date().toUTCString(); // Current time in UTC format
+
+    let imageUrl = ""; // Update image URL to firebase storage
+
+    // Handle image uploads
+    if (file) {
+      const storageRef = sRef(storage, `uploads/${file.name}`);
+      await uploadBytes(storageRef, file);
+      imageUrl = await getDownloadURL(storageRef);
+    }
+
     const messagesRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messagesRef);
-    const currentTime = new Date().toUTCString(); // Current time in UTC format
     await set(newMessageRef, {
       message: postMessage,
       timestamp: currentTime,
+      displayName, // from useState
+      imageUrl,
+      likes: [],
+      userId: auth.currentUser.uid, // Assuming 'auth' is your firebase auth instance
     });
-    setPostMessage(""); // Clear the message input after submitting
+
+    setPostMessage("");
+    setFile(null);
   };
 
   return (
@@ -91,48 +186,107 @@ const App = () => {
         {isLoggedIn ? (
           <>
             <form onSubmit={writeData}>
-              <input
-                type="text"
-                id="post"
-                placeholder="Post a message"
-                onChange={(e) => setPostMessage(e.target.value)}
-                value={postMessage}
-              />
-              <br />
-              <input type="submit" />
+              <FormControl>
+                <FormLabel>Message</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Post a message"
+                  onChange={(e) => setPostMessage(e.target.value)}
+                  value={postMessage}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Upload Image</FormLabel>
+                <Input
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                />
+              </FormControl>
+              <Button type="submit">Submit</Button>
             </form>
-            <button onClick={signOut}>Sign out</button>
+            <Button onClick={handleSignOut}>Sign out</Button>
             <ol>
+              {/* START of the mapping over messages */}
               {messages.map((message) => (
-                <li key={message.key}>
-                  {message.val.message} - {message.val.timestamp}
-                </li>
+                <Box key={message.key} p={4} borderWidth={1} borderRadius="lg">
+                  <Text fontSize="xl">{message.val.displayName}</Text>
+                  <Text>{message.val.message}</Text>
+
+                  <Text>
+                    {message.val.imageUrl && (
+                      <Image
+                        src={message.val.imageUrl}
+                        alt="Uploaded content"
+                        boxSize="400px"
+                        objectFit="cover"
+                      />
+                    )}
+                  </Text>
+
+                  <Button
+                    onClick={() => handleLike(message.key)}
+                    colorScheme={
+                      likedMessages.includes(message.key) ? "teal" : "gray"
+                    }
+                  >
+                    Like
+                  </Button>
+
+                  <FormControl>
+                    {/* Displaying the Comments */}
+                    <FormLabel>Comments</FormLabel>
+                    {message.val.comments &&
+                      message.val.comments.map((comment) => (
+                        <Text key={comment}>{comment}</Text>
+                      ))}
+
+                    {/* Add a Comment Form */}
+                    <FormLabel>Add a Comment</FormLabel>
+                    <Input
+                      type="text"
+                      placeholder="Your comment..."
+                      onChange={(e) =>
+                        setComments({
+                          ...comments,
+                          [message.key]: e.target.value,
+                        })
+                      }
+                      value={comments[message.key] || ""}
+                    />
+                    <Button mt={2} onClick={() => handleComment(message.key)}>
+                      Post Comment
+                    </Button>
+                  </FormControl>
+                </Box>
               ))}
+              {/* END of the mapping over messages */}
             </ol>
           </>
         ) : (
           <>
-            <div>
-              <input
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input
                 type="text"
-                id="email"
                 placeholder="Email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
               />
-              <input
+            </FormControl>
+            <FormControl>
+              <FormLabel>Password</FormLabel>
+              <Input
                 type="password"
-                id="password"
                 placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-            </div>
-            <div>
-              <button onClick={handleSignup}>Sign up</button>
-              <button onClick={handleLogin}>Log in</button>
-              <button onClick={handlePasswordReset}>Forgot password</button>
-            </div>
+            </FormControl>
+            <HStack spacing={4}>
+              <Button onClick={handleSignup}>Sign up</Button>
+              <Button onClick={handleLogin}>Log in</Button>
+              <Button onClick={handlePasswordReset}>Forgot password</Button>
+            </HStack>
           </>
         )}
       </header>
--- src/firebase.js
+++ src/firebase.js
@@ -23,5 +23,6 @@ const authInstance = getAuth(firebaseApp);
 
 // Get a reference to the database service and export the reference for other modules
 export const database = getDatabase(firebaseApp);
+export const storage = getStorage(firebaseApp);
 
 export const auth = getAuth(firebaseApp);
