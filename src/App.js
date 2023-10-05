3a291e30fb4ff4a736a3ef91b4720dec9467637b
root <root@DESKTOP-DT504RQ>
2023-10-06 07:45:09 +0800

    minor adjustment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { get, push, ref, remove, set } from "firebase/database";
+import { push, ref, remove, set } from "firebase/database";
 import { database, auth } from "./firebase";
 import {
   createUserWithEmailAndPassword,
