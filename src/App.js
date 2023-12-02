76ad9e7a0faebf27eceea17609138337fa85fdc3
magiicloud <hongyun_rc@outlook.com>
2023-12-02 11:34:24 +0800

    refactored the chat messages into a component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { onChildAdded, ref } from "firebase/database";
-import { database } from "./firebase";
-import "./App.css";
 import FileUploadForm from "./Components/FileUploadForm";
 import MessageForm from "./Components/MessageForm";
 import PostDisplay from "./Components/PostDisplay";
-
-// Save the Firebase message folder name as a constant to avoid bugs due to misspelling
-const DB_MESSAGES_KEY = "messages";
+import ChatMessages from "./Components/ChatMessages";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 class App extends React.Component {
   constructor(props) {
@@ -15,44 +11,15 @@ class App extends React.Component {
     // Initialise empty messages array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
-      messages: [],
+      loggedInUser: "",
     };
   }
 
-  componentDidMount() {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
-    // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
-      // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
-      }));
-    });
-  }
-
   render() {
-    // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message, index) => (
-      <li
-        key={message.key}
-        className={`chat ${index % 2 === 0 ? "chat-start" : "chat-end"}`} //alternate between chat start and chat end
-      >
-        <div className="chat-footer text-xs opacity-50">{message.val.date}</div>
-        <div className="chat-bubble text-left break-all">
-          {message.val.messageString}
-        </div>
-        <div>
-          <img src={message.val.url} alt={message.val.name} />
-        </div>
-      </li>
-    ));
     return (
       <div className="h-screen flex flex-col items-center justify-end pb-10 lg:flex-row lg:justify-around">
         <div className="">
-          <div className="mb-10">
-            <ol className="">{messageListItems}</ol>
-          </div>
+          <ChatMessages />
           <div className="flex lg:justify-between m-2 mb-10">
             <MessageForm />
           </div>
