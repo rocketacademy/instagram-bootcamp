e9a5f2f1614404f1ba0be3b73a1dd0644b709f60
patrickkok <pkok1996@gmail.com>
2023-12-05 21:34:16 +0800

    Refactor App component as a function component instead of class component

--- src/App.js
+++ src/App.js
@@ -1,74 +1,59 @@
-import React from "react";
 import Textfield from "./Components/Textfield.js";
 import Posts from "./Components/Posts.js";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
-    // When Firebase changes, update local state, which will update local UI
-    this.state = {
-      nameField: "",
-      name: "",
-      screen: false,
-    };
+const App = () => {
+  const [userField, setUserField] = useState("");
+  const [user, setUser] = useState("");
+  const [screen, setScreen] = useState(false);
+  const [displayScreen, setDisplayScreen] = useState(null);
 
-    this.screens = {
-      messages: <Textfield name={this.state.name} />,
-      posts: <Posts name={this.state.name} />,
-    };
-  }
+  useEffect(() => {
+    if (screen === "messages") {
+      setDisplayScreen(<Textfield name={user} />);
+    } else if (screen === "posts") {
+      setDisplayScreen(<Posts name={user} />);
+    }
+  }, [screen, user]);
 
-  componentDidUpdate() {
-    this.screens = {
-      messages: <Textfield name={this.state.name} />,
-      posts: <Posts name={this.state.name} />,
-    };
-  }
-
-  loadItem = (item) => {
-    this.setState({
-      screen: this.state.screen === item ? false : item,
-    });
+  const loadItem = (item) => {
+    setScreen(screen === item ? false : item);
   };
 
-  handleChange = (e, field) => {
-    this.setState({ [field]: e.target.value });
+  const handleChange = (e, setter) => {
+    setter(e.target.value);
   };
 
-  handleSubmit = (e) => {
-    this.setState({ name: this.state.nameField });
+  const handleSubmit = (e) => {
+    setUser(userField);
   };
-
-  render() {
-    return (
-      <div className="App">
-        <div className="footer-nav">
-          <h4 onClick={() => this.loadItem("messages")}>msgs</h4>
-          <h4 onClick={() => this.loadItem("posts")}>posts</h4>
-        </div>
-        {this.state.screen ? (
-          this.screens[this.state.screen]
-        ) : this.state.name === "" ? (
-          <header className="App-header row">
-            <h4>Please insert your name</h4>
-            <input
-              className="name-field"
-              type="text"
-              value={this.state.nameField}
-              onChange={(e) => this.handleChange(e, "nameField")}
-            />
-            <button onClick={this.handleSubmit}>Submit</button>
-          </header>
-        ) : (
-          <header className="App-header row">
-            <h1>Logged in as {this.state.name}</h1>
-          </header>
-        )}
+  return (
+    <div className="App">
+      <div className="footer-nav">
+        <h4 onClick={() => loadItem("messages")}>msgs</h4>
+        <h4 onClick={() => loadItem("posts")}>posts</h4>
       </div>
-    );
-  }
-}
+      {screen ? (
+        displayScreen
+      ) : user === "" ? (
+        <header className="App-header row">
+          <h4>Please insert your name</h4>
+          <input
+            className="name-field"
+            type="text"
+            value={userField}
+            onChange={(e) => handleChange(e, setUserField)}
+          />
+          <button onClick={handleSubmit}>Submit</button>
+        </header>
+      ) : (
+        <header className="App-header row">
+          <h1>Logged in as {user}</h1>
+        </header>
+      )}
+    </div>
+  );
+};
 
 export default App;
