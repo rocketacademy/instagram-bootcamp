ac46616225b1c1c5e52a1ea8cbf367e81dd779ab
Mannushka <zaimar1910@gmail.com>
2023-12-06 08:17:47 +0800

    line 65 + boostrap form added and edited

--- src/Components/AuthForm.js
+++ src/Components/AuthForm.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
-import { InputGroup } from "react-bootstrap";
-
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { auth } from "./firebase";
 import {
   createUserWithEmailAndPassword,
@@ -63,39 +61,76 @@ class AuthForm extends React.Component {
 
   render() {
     return (
-      <div className="error-message">
-        <div>
+      <div>
+        <p>Please sign up / sign in to post.</p>
+        <Form>
+          <Form.Group
+            as={Row}
+            className="mb-3 align-items-center"
+            controlId="formPlaintextEmail"
+          >
+            <Form.Label column sm="4">
+              Email
+            </Form.Label>
+            <Col sm="8">
+              <Form.Control
+                type="text"
+                placeholder="Enter your email"
+                value={this.state.email}
+                onChange={this.setEmail}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3 align-items-center"
+            controlId="formPlaintextPassword"
+          >
+            <Form.Label column sm="4">
+              Password
+            </Form.Label>
+            <Col sm="8">
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                value={this.state.password}
+                onChange={this.setPassword}
+              />
+            </Col>
+          </Form.Group>
+        </Form>
+        <div className="errorMessage">
           <p>{this.state.errorCode && `Error code: ${this.state.errorCode}`}</p>
           <p>
             {this.state.errorMessage &&
               `Error message: ${this.state.errorMessage}`}
           </p>
         </div>
-        <label>Email</label>
-
-        <input
-          type="text"
-          placeholder="Enter your email"
-          value={this.state.email}
-          onChange={this.setEmail}
-        ></input>
-        <br />
-        <label>Password</label>
-
-        <input
-          type="text"
-          placeholder="Enter your password"
-          value={this.state.password}
-          onChange={this.setPassword}
-        ></input>
-        <br />
-        <Button className="mt-2" onClick={this.signIn}>
-          Sign in
-        </Button>
-        <br />
-        <Button className="mt-2 mb-3" onClick={this.signUp}>
-          Sign up
-        </Button>
+        <div>
+          <p className="preLoginMessage">
+            New user? Click "sign up" to register your account.
+          </p>
+          <Button
+            className="mb-4"
+            variant="outline-light"
+            onClick={this.signUp}
+          >
+            Sign up
+          </Button>
+        </div>
+        <div>
+          <p className="preLoginMessage">
+            Already have an account? Click "sign in" to log in.
+          </p>
+          <Button
+            className="mb-4"
+            variant="outline-light"
+            onClick={this.signIn}
+          >
+            Sign in
+          </Button>
+        </div>
       </div>
     );
   }
