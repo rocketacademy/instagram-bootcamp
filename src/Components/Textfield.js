0fcc51efbe4b4d22cfcb99e3efaacf9b53e79e7e
patrickkok <pkok1996@gmail.com>
2023-12-07 20:20:27 +0800

    Refractor Textfield component to use function component instead of class component

--- src/Components/Textfield.js
+++ src/Components/Textfield.js
@@ -1,104 +1,93 @@
 import React from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
 import { database } from "../firebase.js";
+import { useState, useEffect } from "react";
 import "../App.css";
 
 const DB_MESSAGES_KEY = "messages";
 
-class Textfield extends React.Component {
-  constructor(props) {
-    super(props);
-    // Initialise empty messages array in state to keep local state in sync with Firebase
-    // When Firebase changes, update local state, which will update local UI
-    this.state = {
-      inputField: "",
-      messages: [],
-    };
-  }
+const Textfield = (props) => {
+  const [inputField, setInputField] = useState("");
+  const [messages, setMessages] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
+    let loadedMessages = [];
     const messagesRef = ref(database, DB_MESSAGES_KEY);
     // onChildAdded will return data for every child at the reference and every subsequent new child
-    this.unsubscribe = onChildAdded(messagesRef, (data) => {
+    const unsubscribe = onChildAdded(messagesRef, (data) => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      this.setState((state) => ({
-        // Store message key so we can use it as a key in our list items when rendering messages
-        messages: [...state.messages, { key: data.key, val: data.val() }],
-      }));
+      loadedMessages = [...loadedMessages, { key: data.key, val: data.val() }];
+      setMessages(loadedMessages);
     });
-  }
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  handleChange = (e, field) => {
-    this.setState({ [field]: e.target.value });
+  const handleChange = (e, setter) => {
+    setter(e.target.value);
   };
 
-  // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
+  const writeData = () => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
     let newDate = new Date() - 0;
     // newDate = newDate - newDate.getTimezoneOffset() * 60000;
     set(newMessageRef, {
-      text: this.state.inputField,
+      text: inputField,
       date: newDate,
-      name: this.props.name,
+      name: props.name,
     });
-    this.setState({ inputField: "" });
+    setInputField("");
   };
 
-  render() {
-    // Convert messages in state to message JSX elements to render
-    let messageListItems = this.state.messages.map((message) => (
-      <div
-        style={{
-          textAlign: this.props.name === message.val.name ? "right" : "left",
-        }}
-        key={message.key}
-      >
-        <p className="msg-name">
-          {this.props.name === message.val.name ? "You" : message.val.name}
-        </p>
-        <div className="msg-bubble">
-          {message.val.text}
-          <br />
-          <span className="msg-date-time">
-            {new Date(message.val.date).toLocaleTimeString()},
-            {new Date(message.val.date).toLocaleDateString()}
-          </span>
-        </div>
+  let messageListItems = messages.map((message) => (
+    <div
+      style={{
+        textAlign: props.name === message.val.name ? "right" : "left",
+      }}
+      key={message.key}
+    >
+      <p className="msg-name">
+        {props.name === message.val.name ? "You" : message.val.name}
+      </p>
+      <div className="msg-bubble">
+        {message.val.text}
+        <br />
+        <span className="msg-date-time">
+          {new Date(message.val.date).toLocaleTimeString()},
+          {new Date(message.val.date).toLocaleDateString()}
+        </span>
       </div>
-    ));
-    return (
-      <header className="App-header row">
-        <h4>
-          {this.props.name === ""
-            ? "Please insert your name before chatting."
-            : `Currently logged in as: ${this.props.name}`}
-        </h4>
-        <div className="msg-container col-lg-8 col-sm-12">
-          {this.props.name === "" ? null : messageListItems}
-        </div>
+    </div>
+  ));
 
-        {this.props.name !== "" && (
-          <div>
-            <input
-              className="col-lg-8 col-sm-12"
-              type="text"
-              value={this.state.inputField}
-              onChange={(e) => this.handleChange(e, "inputField")}
-            />
-            <button className="col-lg-8 col-sm-12" onClick={this.writeData}>
-              Send
-            </button>
-          </div>
-        )}
-      </header>
-    );
-  }
-}
+  return (
+    <header className="App-header row">
+      <h4>
+        {props.name === ""
+          ? "Please insert your name before chatting."
+          : `Currently logged in as: ${props.name}`}
+      </h4>
+      <div className="msg-container col-lg-8 col-sm-12">
+        {props.name === "" ? null : messageListItems}
+      </div>
+
+      {props.name !== "" && (
+        <div>
+          <input
+            className="col-lg-8 col-sm-12"
+            type="text"
+            value={inputField}
+            onChange={(e) => handleChange(e, setInputField)}
+          />
+          <button className="col-lg-8 col-sm-12" onClick={writeData}>
+            Send
+          </button>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Textfield;
