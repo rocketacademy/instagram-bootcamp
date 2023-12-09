753cbee51ea9806bbb007c15bfb36b2aae36961c
Mannushka <zaimar1910@gmail.com>
2023-12-09 20:31:37 +0800

    router and link added, logic for displayig the app content stored in the app variable

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import { auth } from "./firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import AuthForm from "./AuthForm";
+// import AuthForm from "./AuthForm";
+import NewAuthForm from "./NewAuthForm";
 import PostForm from "./PostForm";
 import Feed from "./Feed";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
-
+import { Route, Routes, Link } from "react-router-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -32,25 +33,42 @@ class App extends React.Component {
     signOut(auth);
     this.setState({ isUserLoggedIn: false, user: null });
   };
+
   render() {
+    const app = (
+      <div>
+        <div>
+          {this.state.isUserLoggedIn ? (
+            <p>
+              Hi {this.state.user.email}! You are now logged in and can post!
+            </p>
+          ) : (
+            <Link to="authform">Please sign up / sign in to post.</Link>
+          )}
+          {this.state.isUserLoggedIn && (
+            <Button
+              variant="outline-light"
+              className="mb-5"
+              onClick={this.signOut}
+            >
+              Sign out
+            </Button>
+          )}
+          {this.state.isUserLoggedIn && <PostForm />}
+        </div>
+        <div className="feed mt-4">
+          <Feed />
+        </div>
+      </div>
+    );
+
     return (
       <div className="App">
         <header>Rocketgram 🚀</header>
-        {this.state.isUserLoggedIn && (
-          <p>Hi {this.state.user.email}! You are now logged in and can post!</p>
-        )}
-        {this.state.isUserLoggedIn && (
-          <Button
-            variant="outline-light"
-            className="mb-5"
-            onClick={this.signOut}
-          >
-            Sign out
-          </Button>
-        )}
-        {this.state.isUserLoggedIn ? <PostForm /> : <AuthForm />}
-
-        <Feed />
+        <Routes>
+          <Route path="/" element={app} />
+          <Route path="authform" element={<NewAuthForm />} />
+        </Routes>
       </div>
     );
   }
