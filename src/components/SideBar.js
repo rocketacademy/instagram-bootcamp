5d49a7427d889e709f1b601aa21a126677a6d553
qiannnly <qianlinggg@gmail.com>
2023-12-11 16:52:34 +0800

    added font awesome icons to represent feed, create post and messenger

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import Posts from "./Posts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEnvelope,
@@ -9,24 +8,21 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => {
-  const [isPostModalOpen, setIsPostModalOpen] = useState(false);
-
   const navigate = useNavigate();
 
   return (
     <>
-      <div class="fixed left-0 h-screen px-6 bg-gray-100 space-y-8 flex-col flex justify-center">
-        <button type="submit" onClick={() => navigate("/")}>
+      <div class="side-bar">
+        <button type="button" onClick={() => navigate("/feed")}>
           <FontAwesomeIcon icon={faBook} />
         </button>
-        <button type="submit" onClick={() => setIsPostModalOpen(true)}>
+        <button type="button" onClick={() => navigate("/feed/post ")}>
           <FontAwesomeIcon icon={faPencil} />
         </button>
-        <button type="submit" onClick={() => navigate("/messenger ")}>
+        <button type="button" onClick={() => navigate("/messenger ")}>
           <FontAwesomeIcon icon={faEnvelope} />
         </button>
       </div>
-      {isPostModalOpen && <Posts setIsPostModalOpen={setIsPostModalOpen} />}
     </>
   );
 };
